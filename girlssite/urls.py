70f5ff0be4e98a55b331c03d0151040c703ee3df
Anastasia <Anastasia2644@yandex.ru>
2017-01-10 01:01:20 +0300

    Changed the HTML  for the site.

--- girlssite/urls.py
+++ girlssite/urls.py
@@ -4,7 +4,10 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
     # url(r'^$', 'girlssite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('blog.urls')),
+
+
 ]
